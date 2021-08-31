2a33f7b2f36d04ca9a0a91ecde7b4c06bd39a213
Omar Ashraf <33941687+omarattia3143@users.noreply.github.com>
2021-08-31 14:06:28 +0200

    init

---
+++ Controllers/ProxyController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using AspNetCore.Proxy;
+using AspNetCore.Proxy.Options;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace ProxyExample.Controllers
+{
+    public class ProxyController : Controller
+    {
+        private static IConfiguration _configuration;
+
+        public ProxyController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("/{rest}")]
+        public Task ProxyCatchAll(string rest)
+        {
+            //to replace wrong encoding from swagger ui parameters
+            var encodedRest = Regex.Replace(rest, @"%2F", "/");
+
+            return this.HttpProxyAsync($"{_configuration["URL"]}/{encodedRest}");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,59 @@
+using AspNetCore.Proxy;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ProxyExample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            }));
+
+            services.AddProxies();
+            services.AddControllers().AddXmlSerializerFormatters().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.WriteIndented = true;
+            });
+
+            services.AddSwaggerGen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProxyExample v1"));
+            }
+
+            // app.UseHttpsRedirection();
+
+            // app.RunProxy();
+            app.UseCors("MyPolicy");
+
+            app.UseRouting();
+            // app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
+        }
+    }
+}
